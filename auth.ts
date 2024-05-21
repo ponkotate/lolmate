e5b57f2cf8aa2d27a5a1652c199e7d22e8dbaaeb
ponkotate <ponkotate@gmail.com>
2024-05-21 18:47:46 +0900

    feat: Prismaの設定

--- auth.ts
+++ auth.ts
@@ -1,12 +1,18 @@
 import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
 import GoogleProvider from 'next-auth/providers/google';
 
+const prisma = new PrismaClient();
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   session: { strategy: 'jwt' },
+  secret: process.env.AUTH_SECRET!,
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!
     })
   ]
 });
