a8d713b8a0b4ccab0da6f87529ea59a84a4790cb
miyokz <miyokawa@outlook.com>
2024-05-21 18:53:22 +0900

    feat:ルーム画面でDynamicRouteを利用するように変更

--- app/room/[uuid]/page.tsx
+++ app/room/[uuid]/page.tsx
@@ -1,9 +1,17 @@
 import Dashboard from '@/components/templates/dashboard';
+import { notFound } from "next/navigation";
 
-export default function Page() {
+export default function Page({ params }: { params: { uuid: string } }) {
+ 
+  const room = true
+
+  if (!room) {
+    return notFound();
+  }
+
   return (
     <Dashboard>
-      <div></div>
+      <div>RoomID: {params.uuid}</div>
     </Dashboard>
   );
 }
